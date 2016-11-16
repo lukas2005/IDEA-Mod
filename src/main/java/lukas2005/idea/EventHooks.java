a83ad2a0b48764333f3095d67bca07328d5d04d9
lukas2005 <lukas2005.38@gmail.com>
2016-11-16 19:04:42 +0100

    trying to create custom player gui

--- src/main/java/lukas2005/idea/EventHooks.java
+++ src/main/java/lukas2005/idea/EventHooks.java
@@ -1,28 +1,39 @@
 package lukas2005.idea;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHooks {
 	
 	@SubscribeEvent
-	public static void onItemTooltip(ItemTooltipEvent e) {
+	public void onContainerOpen(PlayerContainerEvent e) {
 		
-		if (e.getItemStack().getItem() instanceof ItemBlock) {
-		
-			e.getToolTip().add("Weight: "+CustomValues.BLOCKWEIGHT.get(Block.getBlockFromItem(e.getItemStack().getItem())));
+		if (e.getContainer() instanceof Container) {
 			
-		} else if (e.getItemStack().getItem() instanceof Item) {
-			
-			e.getToolTip().add("Weight: "+CustomValues.BLOCKWEIGHT.get(e.getItemStack().getItem()));
+			e.setCanceled(true);
+			e.getEntityPlayer().openGui(IDEAMod.INSTANCE, 1, e.getEntityPlayer().getEntityWorld(), e.getEntityPlayer().getPosition().getX(), e.getEntityPlayer().getPosition().getY(), e.getEntityPlayer().getPosition().getZ());
 			
 		}
 		
-		
-		
+	}
+	
+	@SubscribeEvent
+	public void onItemTooltip(ItemTooltipEvent e) {
+		if (Reference.CONFIG_USE_WEIGHT) {
+			if (e.getItemStack().getItem() instanceof ItemBlock) {
+			
+				e.getToolTip().add("Weight: "+CustomValues.BLOCKWEIGHT.get(Block.getBlockFromItem(e.getItemStack().getItem())));
+				
+			} else {
+				
+				e.getToolTip().add("Weight: "+CustomValues.BLOCKWEIGHT.get(e.getItemStack().getItem()));
+				
+			}
+		}
 	}
 	
 }
---
+++ src/main/java/lukas2005/idea/guis/PlayerGuiContainer.java
@@ -0,0 +1,19 @@
+package lukas2005.idea.guis;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+
+public class PlayerGuiContainer extends GuiContainer {
+
+	public PlayerGuiContainer(Container inventorySlotsIn) {
+		super(inventorySlotsIn);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
